10b16f804658f8b031be7c2a86c7d473b313bd09
unknown <peralesvillanuevaemerson@gmail.com>
2022-07-20 20:42:17 -0600

    script to autoload production enviroment in aws done

--- projects/web-system/src/app/core/views/pages/page-login/page-login.component.ts
+++ projects/web-system/src/app/core/views/pages/page-login/page-login.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../../../../config/injections/layout/layout.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'uwu-page-login',
   templateUrl: './page-login.component.html',
   styleUrls: ['./page-login.component.css'],
 })
 export class PageLoginComponent implements OnInit {
-  constructor(private layoutService: LayoutService) {
+  constructor(private layoutService: LayoutService, private router: Router) {
     this.layoutService.setLayout({ hideHeader: true, hideSidebar: true });
   }
 
   ngOnInit(): void {}
+
+  navigate() {
+    this.router.navigate(['/medic']);
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.layoutService.setLayout({ hideHeader: false, hideSidebar: false });
+  }
 }
